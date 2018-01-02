f9b32c28b45cddd783639bdcddec3e93c33adce9
Ivan Golubev <mail.igolubev@gmail.com>
2018-01-03 00:36:50 +0300

    changes

--- corsbridge.php
+++ corsbridge.php
@@ -1,6 +1,10 @@
 <?php
 header('Cache-Control: no-cache, private');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+//Access-Control-Allow-Headers
+//"Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE,OPTIONS"
 header('Content-Type: application/xml');
 //ini_set('display_errors', 0);
 if (!empty($_REQUEST["db"])) {
